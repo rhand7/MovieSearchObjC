120f25491a0ddfa3368ca87fc57fff4d61385c09
rhand7 <handje@BC-MacBookPro-0587d1.local>
2017-06-22 09:38:57 -0600

    Initial Commit

---
+++ MovieSearchObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MovieSearchObjC
+//
+//  Created by handje on 6/22/17.
+//  Copyright © 2017 Rob Hand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MovieSearchObjC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MovieSearchObjC
+//
+//  Created by handje on 6/22/17.
+//  Copyright © 2017 Rob Hand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
